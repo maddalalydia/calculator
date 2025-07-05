ac5e38250725f7feeca90ad280578ea2dee1954d
maddalalydia <161550402+maddalalydia@users.noreply.github.com>
2025-07-05 14:14:58 +0530

    Add files via upload

---
+++ nirula.py
@@ -0,0 +1,40 @@
+import pandas as pd
+df=pd.read_csv('Diabetes.csv')
+r droping')
+df.isnull().sum()
+print('no of missing values in glucose',df[df['Glucose']==0].shape[0])
+print('no of missing values in BMI',df[df['BMI']==0].shape[0])
+print('no of missing values in BloodPressure',df[df['BloodPressure']==0].shape[0])
+print('no of missing values in Pregnancies',df[df['Pregnancies']==0].shape[0])
+print('no of missing values in SkinThickness',df[df['SkinThickness']==0].shape[0])
+print('no of missing values in Insulin',df[df['Insulin']==0].shape[0]) 
+print('no of missing values in Age',df[df['Age']==0].shape[0]);   
+df['Glucose']=df['Glucose'].replace(0,df['Glucose'].mean())
+df['BMI']=df['BMI'].replace(0,df['BMI'].mean())
+df['BloodPressure']=df['BloodPressure'].replace(0,df['BloodPressure'].mean())
+df['Pregnancies']=df['Pregnancies'].replace(0,df['Pregnancies'].mean())
+df['SkinThickness']=df['SkinThickness'].replace(0,df['SkinThickness'].mean())
+df['Insulin']=df['Insulin'].replace(0,df['Insulin'].mean())
+df['Age']=df['Age'].replace(0,df['Age'].mean())
+print('no of missing values in glucose',df[df['Glucose']==0].shape[0])
+print('no of missing values in BMI',df[df['BMI']==0].shape[0])
+print('no of missing values in BloodPressure',df[df['BloodPressure']==0].shape[0])
+print('no of missing values in Pregnancies',df[df['Pregnancies']==0].shape[0])
+print('no of missing values in SkinThickness',df[df['SkinThickness']==0].shape[0])
+print('no of missing values in Insulin',df[df['Insulin']==0].shape[0]) 
+print('no of missing values in Age',df[df['Age']==0].shape[0])
+print(df.describe())
+x=df.drop(columns='Outcome',axis=1)
+x=df['Age','BloodPressure','BMI']
+y=df['Outcome']
+print(x.head())
+print(y.head())
+print(df.head())
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+scaler.fit(x)
+sd=scaler.transform(x)
+x=sd
+y=df.Outcome
+print(x)
+print(y)
